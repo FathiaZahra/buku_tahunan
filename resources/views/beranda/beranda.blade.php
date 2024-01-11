43df10a267a9e88406e2e3932333262699472f69
FathiaZahra <124228711+FathiaZahra@users.noreply.github.com>
2024-01-11 14:29:19 +0700

    fathia

---
+++ resources/views/beranda/beranda.blade.php
@@ -0,0 +1,32 @@
+@extends('layout.index')
+@section('content')
+
+
+    <div style='margin-left:70px; margin-top:40px;'>
+        <span class="h1">Visi</span>
+    </div>
+
+    <div style='margin-left:70px; margin-top:15px;'>
+        <span class="h6 text-white">SMK NEGERI 1 KOTA BEKASI</span>
+    </div>
+
+    <div class="card" style='margin-left:70px; margin-top:15px; width:1350px;'>
+        <div class="card-body text-center h5">
+            “FOKUS MENJADI JODOH JENO”
+        </div>
+      </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
