5fc7810281b77be9c584908cf5995579af7057a9
kesyaa1123 <kesyaafiana30@gmail.com>
2024-01-25 11:20:56 +0700

    narik

--- resources/views/beranda/beranda.blade.php
+++ resources/views/beranda/beranda.blade.php
@@ -36,29 +36,193 @@
     </div>
 
 
-    <div style='margin-left:70px; margin-top:15px; width:1000px;'>
+    <div style='margin-left:70px; margin-top:100px; width:1000px;'>
         <div>
-            <h1>SAMBUTAN KEPALA SEKOLAH</h1>
-            <div style='margin-left:70px; margin-top:15px;'>
-                <span class="h6 text-white">SMK NEGERI 1 KOTA BEKASI</span>
-            </div>
-            <p>Dr. Lee Soo Man, S.M</p>
-            <p>Assalamualaikum wrwb. Hamdan wa syukron lillah, ama ba'du.</p>
-            <p>Segala puji bagi dzat yang telah melimpahkan rahmat kepada kita semua,</p>
-            <p>shalawat dan salam tetap tercurah kepada baginda Nabi Muhammad SAW, yang</p>
-            <p>telah membimbing dan menunjukkan kita jalan kebenaran yakni addinul Islam.</p>
-            <p>Dan Semoga kita kelak mendapatkan syafaatnya, Aamiin.</p>
-            <p>Saya hanya bisa mendoakan, semoga selepas dari kelulusan Ananda Jeno Lee</p>
-            <p>dari SMK NEO CULTURE, Ananda semakin saleh, semakin cerdas, dan siap memimpin.</p>
-            <p>Dijalur apapun dan dibidang apapun semoga diberikan kesuksesan dan mendapat ridho dari Allah SWT.</p>
-            <p>Aamiin.</p>
-            <p>Selamat menghadapi dunia nyata, dunia yang penuh ujian dan tantangan.</p>
-            <p>Semoga engkau mampu melewatinya dengan baik.</p>
-            <p>Wassalamulalaikum wr wb.</p>
+
+        <div 
+            style="
+            display: flex;
+            ">   
+            <div
+                style="
+                        background-image: url('{{ asset('gambar/kpl_sekolah.jpg')}}');
+                        background-repeat: no-repeat;
+                        height:550px;
+                        width: 500px;
+                        padding: 12ppx;
+                        display: flex;
+                        justify-content: center;
+                        ">
+                        <img src=" {{ asset('gambar/kpl_sekolah')}}" alt="" class="w-20 h-20">
+                        <br>          
+        </div>
+        <div>
+        <div style='margin-left:70px; margin-top:50px;'>
+            <span class="h6 text-white relative margin-top:20px;">SMK NEGERI 1 KOTA BEKASI</span>
+        </div>
+        <div style='margin-left:70px; margin-top:30px;'>
+            <h1>SAMBUTAN</h1> <br>
+            <h1>KEPALA</h1> <br>
+            <h1>SEKOLAH</h1> <br>
+        </div>
+        <div style='margin-left:70px; margin-top:30px;'>
+            <span class="h6 text-white relative margin-top:20px;">Drs.Lee Soo Man, S.M</span>
+        </div>   
+    </div>
+    </div> 
+    <div class="card" style='margin-left:1px; margin-top:15px; width:1000px;'>
+        <div class="card-body h5">
+            Assalamualaikum wrwb. Hamdan wa syukron lillah, ama ba’du. <br>
+            Segala puji bagi dzat yang telah melimpahkan rahmat kepada kita semua, shalawat dan salam tetap 
+            tercurah kepada baginda Nabi Muhammad SAW, yang telah membimbing dan menunjukkan kita jalan kebenaran
+            yakni addinul Islam. Dan Semoga kita kelak mendapatkan syafaatnya, Aamiin. <br>
+            Saya hanya bisa mendoakan, semoga selepas dari kelulusan Ananda Jeno Lee dari SMK NEO CULTURE, 
+            Ananda semakin saleh, semakin cerdas, dan siap memimpin. Dijalur apapun dan dibidang apapun semoga 
+            diberikan kesuksesan dan mendapat ridho dari Allah SWT. Aamiin. <br>
+            Selamat menghadapi dunia nyata, dunia yang penuh ujian dan tantangan. Semoga engkau mampu
+             melewatinya dengan baik.Wassalamulalaikum wr wb.
         </div>
     </div>
 
+    <div style='margin-left:1px; margin-top:100px; width:1000px;'>
+        <div>
+    <div 
+    style="
+    display: flex;
+    ">   
+    <div
+        style="
+                background-image: url('{{ asset('gambar/kesiswaan.jpg')}}');
+                background-repeat: no-repeat;
+                height:550px;
+                width: 500px;
+                padding: 12ppx;
+                display: flex;
+                justify-content: center;
+                ">
+                <img src=" {{ asset('gambar/kpl_sekolah')}}" alt="" class="w-20 h-20">
+                <br>          
+</div>
+<div>
+    <div style='margin-left:70px; margin-top:50px;'>
+        <span class="h6 text-white relative margin-top:20px;">SMK NEGERI 1 KOTA BEKASI</span>
+    </div>
+    <div style='margin-left:70px; margin-top:30px;'>
+        <h1>SAMBUTAN</h1><br>
+        <h1>KESISWAAN</h1><br>
+    </div>
+    <div style='margin-left:70px; margin-top:30px;'>
+        <span class="h6 text-white relative margin-top:20px;">Dr. Yang Hyun Suk, Y.G</span>
+    </div>   
+</div>
+</div> 
+<div class="card" style='margin-left:1px; margin-top:15px; width:1000px;'>
+    <div class="card-body h5">
+        Assalamualaikum wrwb. Hamdan wa syukron lillah, ama ba’du. <br>
+        Segala puji bagi dzat yang telah melimpahkan rahmat kepada kita semua, shalawat dan salam tetap 
+        tercurah kepada baginda Nabi Muhammad SAW, yang telah membimbing dan menunjukkan kita jalan kebenaran
+        yakni addinul Islam. Dan Semoga kita kelak mendapatkan syafaatnya, Aamiin. <br>
+        Saya hanya bisa mendoakan, semoga selepas dari kelulusan Ananda Jeno Lee dari SMK NEO CULTURE, 
+        Ananda semakin saleh, semakin cerdas, dan siap memimpin. Dijalur apapun dan dibidang apapun semoga 
+        diberikan kesuksesan dan mendapat ridho dari Allah SWT. Aamiin. <br>
+        Selamat menghadapi dunia nyata, dunia yang penuh ujian dan tantangan. Semoga engkau mampu
+         melewatinya dengan baik.Wassalamulalaikum wr wb.
+    </div>
+</div>
 
+<div style='margin-left:1px; margin-top:100px; width:1000px;'>
+    <div>
+<div 
+style="
+display: flex;
+">   
+<div
+    style="
+            background-image: url('{{ asset('gambar/ketua_bts.jpg')}}');
+            background-repeat: no-repeat;
+            height:550px;
+            width: 500px;
+            padding: 12ppx;
+            display: flex;
+            justify-content: center;
+            ">
+            <img src=" {{ asset('gambar/kpl_sekolah')}}" alt="" class="w-20 h-20">
+            <br>          
+</div>
+<div>
+    <div style='margin-left:70px; margin-top:50px;'>
+        <span class="h6 text-white relative margin-top:20px;">SMK NEGERI 1 KOTA BEKASI</span>
+    </div>
+    <div style='margin-left:70px; margin-top:30px;'>
+        <h1>SAMBUTAN</h1><br>
+        <h1>KETUA</h1><br>
+        <h1>BTS</h1><br>
+    </div>
+    <div style='margin-left:70px; margin-top:30px;'>
+        <span class="h6 text-white relative margin-top:20px;">CHOI SIWON</span>
+    </div>   
+</div>
+</div> 
+<div class="card" style='margin-left:1px; margin-top:15px; width:1000px;'>
+    <div class="card-body h5">
+        Assalamualaikum wrwb. Hamdan wa syukron lillah, ama ba’du. <br>
+        Segala puji bagi dzat yang telah melimpahkan rahmat kepada kita semua, shalawat dan salam tetap 
+        tercurah kepada baginda Nabi Muhammad SAW, yang telah membimbing dan menunjukkan kita jalan kebenaran
+        yakni addinul Islam. Dan Semoga kita kelak mendapatkan syafaatnya, Aamiin. <br>
+        Saya hanya bisa mendoakan, semoga selepas dari kelulusan Ananda Jeno Lee dari SMK NEO CULTURE, 
+        Ananda semakin saleh, semakin cerdas, dan siap memimpin. Dijalur apapun dan dibidang apapun semoga 
+        diberikan kesuksesan dan mendapat ridho dari Allah SWT. Aamiin. <br>
+        Selamat menghadapi dunia nyata, dunia yang penuh ujian dan tantangan. Semoga engkau mampu
+         melewatinya dengan baik.Wassalamulalaikum wr wb.
+    </div>
+</div>
+
+<div style='margin-left:1px; margin-top:100px; width:1000px;'>
+    <div>
+<div 
+style="
+display: flex;
+">   
+<div
+    style="
+            background-image: url('{{ asset('gambar/ketua_osis.jpg')}}');
+            background-repeat: no-repeat;
+            height:550px;
+            width: 500px;
+            padding: 12ppx;
+            display: flex;
+            justify-content: center;
+            ">
+            <img src=" {{ asset('gambar/kpl_sekolah')}}" alt="" class="w-20 h-20">
+            <br>          
+</div>
+<div>
+    <div style='margin-left:70px; margin-top:50px;'>
+        <span class="h6 text-white relative margin-top:20px;">SMK NEGERI 1 KOTA BEKASI</span>
+    </div>
+    <div style='margin-left:70px; margin-top:30px;'>
+        <h1>SAMBUTAN</h1><br>
+        <h1>KETUA</h1><br>
+        <h1>OSIS</h1><br>
+    </div>
+    <div style='margin-left:70px; margin-top:30px;'>
+        <span class="h6 text-white relative margin-top:20px;">KWON JI YONG</span>
+    </div>   
+</div>
+</div> 
+<div class="card" style='margin-left:1px; margin-top:15px; width:1000px;'>
+    <div class="card-body h5">
+        Assalamualaikum wrwb. Hamdan wa syukron lillah, ama ba’du. <br>
+        Segala puji bagi dzat yang telah melimpahkan rahmat kepada kita semua, shalawat dan salam tetap 
+        tercurah kepada baginda Nabi Muhammad SAW, yang telah membimbing dan menunjukkan kita jalan kebenaran
+        yakni addinul Islam. Dan Semoga kita kelak mendapatkan syafaatnya, Aamiin. <br>
+        Saya hanya bisa mendoakan, semoga selepas dari kelulusan Ananda Jeno Lee dari SMK NEO CULTURE, 
+        Ananda semakin saleh, semakin cerdas, dan siap memimpin. Dijalur apapun dan dibidang apapun semoga 
+        diberikan kesuksesan dan mendapat ridho dari Allah SWT. Aamiin. <br>
+        Selamat menghadapi dunia nyata, dunia yang penuh ujian dan tantangan. Semoga engkau mampu
+         melewatinya dengan baik.Wassalamulalaikum wr wb.
+    </div>
+</div>
 
 
 
